104ea557171298c44070979483d4453e2fe88a52
HLC_2021 <HLC_2021@DESKTOP-7Q0HBGC>
2024-01-18 20:03:07 +0700

    Init Source code

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+      static void Main(string[] agrs) {
+
+            Console.WriteLine(agrs[0]);
+        }
+    }
+}
